49316ef5b72b88652d9d1f0e59471725dda0eb42
m_mitin <maximus405@gmail.com>
2022-08-25 23:57:13 +0300

    adding List view

---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Maxim Mitin on 25.08.22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
