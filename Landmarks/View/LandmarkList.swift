26b9385e865ed61a018b569fd2b9a7fe80c69f46
m_mitin <maximus405@gmail.com>
2022-08-26 00:21:05 +0300

    some fixes passing data through

--- Landmarks/View/LandmarkList.swift
+++ Landmarks/View/LandmarkList.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach (["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { devicename in
+                LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: devicename))
+                .previewDisplayName(devicename)
+        }
     }
 }
